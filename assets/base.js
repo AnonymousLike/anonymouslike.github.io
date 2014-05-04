b39d49681ada88edea81e68edfb66e4c61185f44
James Peter <james@melon.com.au>
2014-05-04 04:50:36 -0400

    Home page UX

--- assets/base.js
+++ assets/base.js
@@ -31,7 +31,7 @@ var app = {
                     var img_src = "http://api.url2png.com/v6/P53657257D7555/"+token+"/png/"+img_path;
 
                     // Insert the page data into the recently liked list
-                    $( "#liked" ).append( "<div class='page'><img src='"+img_src+"' /><a class='title' href='" + url + "'>" + item.get('title') + "</a><div class='count'><i class='fa fa-lock'></i> " + item.get('count') + "<div class='nub'><s></s><i></i></div></div></div>" );
+                    $( "#liked" ).append( "<div class='page'><img src='"+img_src+"' /><div class='bg-fade'></div><a class='title' href='" + url + "'>" + item.get('title') + "</a><div class='count'><i class='fa fa-lock'></i> &nbsp;" + item.get('count') + "<div class='nub'><s></s><i></i></div></div></div>" );
 
                 });
                 $('#liked img').error(function(){
